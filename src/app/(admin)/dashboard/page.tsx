471f52749cbec40c559f38739d72ea986c3e5630
anastasiia-kushch <anastasiia.kushch02@gmail.com>
2024-07-05 17:50:06 +0200

    finish error handling

--- src/app/(admin)/dashboard/page.tsx
+++ src/app/(admin)/dashboard/page.tsx
@@ -1,7 +1,10 @@
+import MagicButton from '@/app/components/magic-button';
+
 export default function Home() {
   return (
     <main>
       <h1 className="text-xl">Dashboard page</h1>
+      <MagicButton />
     </main>
   );
 }
