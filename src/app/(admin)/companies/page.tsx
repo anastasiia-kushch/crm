0dc78d4fdbb778746b53604fe3685fccbcd54088
anastasiia-kushch <anastasiia.kushch02@gmail.com>
2024-07-05 15:23:45 +0200

    finish navigation

--- src/app/(admin)/companies/page.tsx
+++ src/app/(admin)/companies/page.tsx
@@ -11,20 +11,22 @@ export default function Page() {
   return (
     <>
       <Header>Companies</Header>
-      <Toolbar action={<AddCompanyButton />}>
-        <SearchInput />
-      </Toolbar>
-      <CompanyTable>
-        <CompanyRow
-          id={1}
-          category={'Products'}
-          company={'Costco'}
-          status={Status.Pending}
-          promotion={true}
-          country={'USA'}
-          joinedDate={'02.19.2023'}
-        />
-      </CompanyTable>
+      <main>
+        <Toolbar action={<AddCompanyButton />}>
+          <SearchInput />
+        </Toolbar>
+        <CompanyTable>
+          <CompanyRow
+            id={1}
+            category={'Products'}
+            company={'Costco'}
+            status={Status.Pending}
+            promotion={true}
+            country={'USA'}
+            joinedDate={'02.19.2023'}
+          />
+        </CompanyTable>
+      </main>
     </>
   );
 }
