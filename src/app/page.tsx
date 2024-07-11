79e090410ebe463a3824d75a615d76aafbc9b491
anastasiia-kushch <anastasiia.kushch02@gmail.com>
2024-07-11 14:46:46 +0200

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Button from './components/button';
 export default function Home() {
   return (
     <main className="px-10 py-6 flex flex-col">
-      <h1 className="text-3xl font-semibold text-gray-900 mb-8 mt-[180px] text-center">
+      <h1 className="text-3xl font-semibold text-gray-900 mb-8 mt-[140px] text-center">
         Home page
       </h1>
       <div className="text-xl mb-14 mx-auto">
