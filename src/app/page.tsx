8931afadea2b4c844dc1ea71cb5b8d007e248e6b
anastasiia-kushch <anastasiia.kushch02@gmail.com>
2024-07-11 12:54:54 +0200

    finish

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-
 export default function Home() {
   return (
     <main>
       <h1 className="text-xl">Home page</h1>
-      <ReactQueryDevtools initialIsOpen={false} />
     </main>
   );
 }
